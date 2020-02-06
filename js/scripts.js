99ff56caa3ec01f03a41f7a7105ab9dceca0c8c8
Sean R Reid <sean@torchcodelab.com>
2020-02-06 10:47:14 -0500

    Looping through nodelist

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const generateListButton = document.querySelector('#generateList'),
+  shoppingListInputs = document.querySelectorAll('.input');
+
+generateListButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  shoppingListInputs.forEach(function(input) {
+    console.log(input.value);
+  });
+});
