787d042af52e5d285a1f2beb9d1fb5e072ca9f61
Sean R Reid <sean@torchcodelab.com>
2020-02-06 09:43:26 -0500

    used createElement() method to append to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,19 @@ const generateList = document.querySelector('#generateList'),
   myList = document.querySelector('#myList'),
   itemList = document.querySelectorAll('input');
 
-function createListFromInput(list) {}
+function createListFromInput(list) {
+  const listElement = document.createElement('ul');
+
+  list.forEach(function(input) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = input.value;
+    listElement.appendChild(listItem);
+  });
+
+  return listElement;
+}
 
 generateList.addEventListener('click', function(e) {
-  console.log('clicked');
+  const theList = createListFromInput(itemList);
+  myList.append(theList);
 });
