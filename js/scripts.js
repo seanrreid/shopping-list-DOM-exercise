d701f46391847557f2c16bd37c02d847f7d3fc40
Sean R Reid <sean@torchcodelab.com>
2020-02-06 08:59:48 -0500

    Working on Solution

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const generateList = document.querySelector('#generateList'),
+  myList = document.querySelector('#myList'),
+  itemList = document.querySelectorAll('input');
+
+function createListFromInput(list) {}
+
+generateList.addEventListener('click', function(e) {
+  console.log('clicked');
+});
