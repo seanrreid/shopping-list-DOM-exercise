4c377a84c19c7334908b00bc18be68f2fc9a1216
Sean R Reid <sean@torchcodelab.com>
2020-02-06 11:21:23 -0500

    Added function to create an eelement

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const generateListButton = document.querySelector('#generateList'),
-  shoppingListInputs = document.querySelectorAll('.input');
+  shoppingListInputs = document.querySelectorAll('.input'),
+  myListDiv = document.querySelector('#myList');
+
+function generateMyList() {
+  const newText = document.createElement('p');
+  newText.innerHTML = 'Hello World';
+  myListDiv.appendChild(newText);
+}
 
 generateListButton.addEventListener('click', function(event) {
   event.preventDefault();
   shoppingListInputs.forEach(function(input) {
     console.log(input.value);
   });
+  generateMyList();
 });
