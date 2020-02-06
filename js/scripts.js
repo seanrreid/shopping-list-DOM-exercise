21974f39fec977a85de5af2837ae92a983c4dc23
Sean R Reid <sean@torchcodelab.com>
2020-02-06 12:00:13 -0500

    Added an additional function to render name and list

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,41 @@
 'use strict';
 
+const randomArray = [{ value: 'Foo' }, { value: 'Bar' }, { value: 'Baz' }];
+
 const generateListButton = document.querySelector('#generateList'),
   shoppingListInputs = document.querySelectorAll('.input'),
   myListDiv = document.querySelector('#myList');
 
-function generateMyList() {
-  shoppingListInputs.forEach(function(input) {
-    const newText = document.createElement('p');
-    newText.innerHTML = input.value;
-    myListDiv.appendChild(newText);
+function generateMyList(list) {
+  const listElement = document.createElement('ul');
+
+  list.forEach(function(input) {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = input.value;
+    listElement.appendChild(listItem);
   });
+
+  return listElement;
+}
+
+function renderNamedList(listElement, listName) {
+  const container = document.createElement('div');
+  const title = document.createElement('h1');
+  title.innerHTML = listName;
+  container.appendChild(title);
+  container.appendChild(listElement);
+
+  myListDiv.appendChild(container);
 }
 
 generateListButton.addEventListener('click', function(event) {
   event.preventDefault();
-  generateMyList();
+  const theList = generateMyList(shoppingListInputs);
+  const randomList = generateMyList(randomArray);
+
+  // myListDiv.appendChild(theList);
+  // myListDiv.appendChild(randomList);
+
+  renderNamedList(theList, 'Food');
+  renderNamedList(randomList, 'Random Array');
 });
